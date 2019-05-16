414154e3bfa9835917bbf9e184b0214322788f68
Yyrii <krzysiek.jurczynski96@gmail.com>
2019-05-17 00:16:00 +0200

    reduced to 3 buttons

--- interface/widgets.py
+++ interface/widgets.py
@@ -20,42 +20,33 @@ class StartPage(tk.Frame):
         self.pack()
         self.canvas = Canvas(self)
 
-        # play button
         self.Show_but = tk.Button(self,text='Show', command = lambda: self.add_canvas())
         self.Show_but.pack()
 
         self.Hide_but = tk.Button(self, text='Hide', command=lambda: self.hide_canvas())
         self.Hide_but.pack()
 
-        self.Play_but = tk.Button(self, text='Play', command=lambda: None)
+        self.Play_but = tk.Button(self, text='Play')
         self.Play_but.pack()
+        self.Play_but.bind('<ButtonPress-1>', self.play_start)
+        self.Play_but.bind('<ButtonRelease-1>', self.play_stop)
 
-        self.Play2_but = tk.Button(self, text='Play2', command=lambda: None)
-        self.Play2_but.pack()
-
-        self.button = tk.Button(self, text="forward")
-        self.button.pack()
-        self.button.bind('<ButtonPress-1>', self.start_motor)
-        self.button.bind('<ButtonRelease-1>', self.stop_motor)
 
     running = False
-
-    def start_motor(self,event):
+    def play_start(self, event):
         global running
         running = True
         sound.waveshape_APP()
 
-    def stop_motor(self,event):
+    def play_stop(self, event):
         global running
         running = False
-
 
 
     def add_canvas(self):
         self.canvas.show()
     def hide_canvas(self):
         self.canvas.hide()
-
 
 
 
